1a71fefc8c17e6334f827cfb022dab1ff9da3d9a
Kobe Van Genechten <kobevg@hotmail.com>
2018-10-18 09:35:31 +0200

    no message

---
+++ index.js
@@ -0,0 +1,5 @@
+var MapboxClient = require('mapbox');
+
+<script src='https://unpkg.com/mapbox@1.0.0-beta9/dist/mapbox-sdk.min.js'></script>
+
+var client = new MapboxClient('pk.eyJ1Ijoia29iZXZnIiwiYSI6ImNqbjRib2I1eDJvYmgzd3F2MGhpdm92cXYifQ.yiImgYEv2XOx8v8EcqCHhA');
